2f2b6b4bd28c163dd870664ad4eb3cbefc483db0
KalebJohnson <Kaleb.Johnson45@yahoo.com>
2021-04-22 13:57:24 -0500

    point system, displayed and ability to gather points to add length

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 import pygame
 from pygame.locals import *
 import time
+import random
 
 SIZE = 40
+
+class Point:
+    def __init__(self, parent_screen):
+        self.image = pygame.image.load('resources/Lol_circle.png').convert()
+        self.parent_screen = parent_screen
+        self.x, self.y = SIZE * 3, SIZE * 3
+    
+    def draw(self):
+        self.parent_screen.blit(self.image, (self.x, self.y))
+        pygame.display.flip()
+
+    def move(self):
+        self.x = random.randint(0,24)*SIZE
+        self.y = random.randint(0,19)*SIZE
 
 class Snake:
     def __init__(self, surface, length):
@@ -12,6 +27,11 @@ class Snake:
         self.length = length
         self.x, self.y = [40]*length, [40]*length
         self.direction = 'down'
+    
+    def grow(self):
+        self.length += 1
+        self.x.append(-1)
+        self.y.append(-1)
 
     def draw(self):
         self.parent_screen.fill((110,110,5))
@@ -50,9 +70,32 @@ class Snake:
 class Game:
     def __init__(self):
         pygame.init()
-        self.surface = pygame.display.set_mode((1200, 1200))
-        self.snake = Snake(self.surface, 4)
+        self.surface = pygame.display.set_mode((1000, 1000))
+        self.snake = Snake(self.surface, 2)
         self.snake.draw()
+        self.point = Point(self.surface)
+        self.point.draw()
+    
+    def collision(self, x1, y1, x2, y2):
+        if x1 >= x2 and x1 < x2 + SIZE:
+            if y1 >= y2 and y1 < y2 + SIZE:
+                return True
+        return False
+
+    def score(self):
+        font = pygame.font.SysFont('arial',30)
+        score = font.render(f"score: {self.snake.length}", True, (255,255,255)) 
+        self.surface.blit(score, (800,10))
+
+    def play(self):
+        self.snake.walk()
+        self.point.draw()
+        self.score()
+        pygame.display.flip()
+
+        if self.collision(self.snake.x[0], self.snake.y[0], self.point.x, self.point.y):
+            self.point.move()
+            self.snake.grow()
 
     def run(self):
         running = True
@@ -77,7 +120,7 @@ class Game:
                     elif event.type == QUIT:
                         running = False
         
-                self.snake.walk()
+                self.play()
                 time.sleep(0.2)
 
 
